120ba37c1831b85c3c2f566e9fc9be1283a0f73a
DEV1\Leandro <leandro.sardi@expandedventure.com>
2021-12-29 13:36:14 -0300

    improving regex to find URLs + improve .gitignore + fix glitch in examples

--- examples/example02.rb
+++ examples/example02.rb
@@ -1,3 +1,3 @@
 require_relative '../lib/blackstack_commons'
 
-BlackStack::Netting::api_call('https://connectionsphere.com/api1.4/ping.json')
+BlackStack::Netting::api_call('https://connectionsphere.com/api1.3/pampa/ping.json')
